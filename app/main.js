835d92ce4b3f06b2b25e6e509f34459e537f6507
murilo-l1 <murilolusvarghi@alunos.utfpr.edu.br>
2024-05-21 15:10:05 -0300

    pass 3rd stage

--- app/main.js
+++ app/main.js
@@ -1,13 +1,28 @@
 const net = require("net");
-
+//curl -i -X GET http://localhost:4221/index.html
 const okResponse = "HTTP/1.1 200 OK\r\n\r\n";
+const notFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
 
 const server = net.createServer((socket) => {
-    socket.write(okResponse);
+    //writeOkMessage(socket);
     socket.on("close", () => {
      socket.end();
      server.close();
    });
+    socket.on("data", (data) => {
+       const request = data.toString();
+        //console.log(request);
+        if(request.startsWith("GET /")){
+           writeSocketMessage(socket, okResponse);
+       }else{
+           writeSocketMessage(socket, notFoundResponse);
+       }
+    });
  });
 
 server.listen(4221, "localhost");
+
+const writeSocketMessage = (socket, message) => {
+    socket.write(message);
+    socket.end();
+}
